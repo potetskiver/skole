6d8268de0319ad336e0af759dc2fe59eb652db02
Alexander Andersen <alexander.andersen@icloud.com>
2026-02-12 08:54:50 +0100

    alo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,13 @@
+import Profile from "@/components/Profile";
+
 export default function Header() {
-  const name = "Gjest";
+  //const name = "Gjest";
 
   return (
     <header style={{ background: "#000", color: "#fff", padding: "12px 0" }}>
       <div style={{ maxWidth: "1024px", margin: "0 auto", padding: "0 16px", display: "flex", alignItems: "center", justifyContent: "space-between" }}>
         <h1 style={{ margin: 0, lineHeight: 1 }}>Skole Header</h1>
-        <div style={{ display: "flex", alignItems: "center", gap: "12px" }}>
-          <h2 style={{ margin: 0, lineHeight: 1 }}>Velkommen {name}</h2>
-        </div>
+        <Profile />
       </div>
     </header>
   );
---
+++ components/Profile.tsx
@@ -0,0 +1,9 @@
+export default function Profile(){
+    const name = "Hello"
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h1 style={{ margin: 0, lineHeight: 1 }}>Velkommen {name}</h1>
+        </div>
+    )
+}
